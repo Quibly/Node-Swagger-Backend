0466dc95597144fb08cd9fab385f3f66f8e82f7c
Quibly <trippy524@yahoo.com>
2022-04-30 14:42:57 -0600

    rest client link updates

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,14 +1,30 @@
 const express = require('express');
+const { ObjectId } = require('mongodb');
 const route = express.Router();
 let connect = require('../db/connect');
+const ObjectID = require('mongodb').ObjectId;
 
 route.get('/', (req, res) => {
     
-    connect.getCollection().find().toArray((err, result) => {
-        if (err) throw err;
-        res.json(result);
-        console.log('Contacts Query Successful')
-        });
+    const results = connect.getCollection().find();
+    
+    results.toArray().then((documents) =>  {
+        res.status(200).json(documents);
+        console.log('Returned All Contacts');
+    });
+    
+});
+
+route.get('/:id', (req, res) => {
+    
+    const contactId = new ObjectId(req.params.id);
+    const results = connect.getCollection().find({_id: contactId});
+    
+    results.toArray().then((documents) =>  {
+        res.status(200).json(documents[0]);
+        console.log(`Returned One Contact ${req.params.id}`);
+    });
+    
 });
 
 module.exports = route;
