e2d9f98165b0cc881edcaf82d656ab36aa081f2a
Quibly <trippy524@yahoo.com>
2022-04-30 15:03:48 -0600

    cleanup and comments

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,20 +1,23 @@
-// load modules
+// load modules for routing and getCollection() function from ../db/connect
 const express = require('express');
-const { ObjectId } = require('mongodb');
 const route = express.Router();
 let connect = require('../db/connect');
 
+// create variable for ObjectId use
+const { ObjectId } = require('mongodb');
+
+// route endpoint for displaying All contacts
 route.get('/', (req, res) => {
     
     const results = connect.getCollection().find();
     
     results.toArray().then((documents) =>  {
         res.status(200).json(documents);
-        console.log('Returned All Contacts');
     });
     
 });
 
+// route endpoint for displaying a Specific contact
 route.get('/:id', (req, res) => {
     
     const contactId = new ObjectId(req.params.id);
@@ -22,7 +25,6 @@ route.get('/:id', (req, res) => {
     
     results.toArray().then((documents) =>  {
         res.status(200).json(documents[0]);
-        console.log(`Returned One Contact ${req.params.id}`);
     });
     
 });
