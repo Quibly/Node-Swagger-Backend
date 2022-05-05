5dd641880f3f1cec936dabde4d7f5b9881d65e13
Quibly <trippy524@yahoo.com>
2022-05-04 22:59:30 -0600

    corrected lint errors

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const route = express.Router();
+// const express = require('express');
+// const route = express.Router();
 let connect = require('../db/connect');
 
 // load model
@@ -9,80 +9,64 @@ let contact = require('../models/contact');
 const { ObjectId } = require('mongodb');
 
 //function for displaying ALL contacts
-function getAll() {
-
+function getAll(request, response) {
     const results = connect.getCollection().find();
-    
-    results.toArray().then((documents) =>  {
-        res.status(200).json(documents);
+    results.toArray().then((documents) => {
+        response.status(200).json(documents);
     });
-
 }
 
 //function for displaying ONE contact
-function getOne() {
-
-    const contactId = new ObjectId(req.params.id);
-    const results = connect.getCollection().find({_id: contactId});
-    
-    results.toArray().then((documents) =>  {
-        res.status(200).json(documents[0]);
+function getOne(request, response) {
+    const contactId = new ObjectId(request.params.id);
+    const results = connect.getCollection().find({ _id: contactId });
+    results.toArray().then((documents) => {
+        response.status(200).json(documents[0]);
     });
-
 }
 
-
 //function for creating a new contact
-async function createContact (request, response) {
-
+async function createContact(request, response) {
     console.log(request.body);
     const newContact = new contact(request.body);
 
     try {
-        await newContact.save()
+        await newContact.save();
         response.status(201).send(`Created Contact Successfully: \n${newContact}`);
     } catch (err) {
-        res.status(500).send(err);
+        response.status(500).send(err);
     }
-
 }
 
-
 //function for updating a contact
-function updateContact (request, response) {
-
-    const {id: _id} = request.params;
+function updateContact(request, response) {
+    const { id: _id } = request.params;
     const content = request.body;
 
     console.log(content);
 
-    contact.findByIdAndUpdate(_id, content, (err) => {
-            if (err) {
-                response.error(err);
-            } else {
-                response.status(200).send(`Updated Contact ID: ${_id}`);
-            }
+    contact.findByIdAndUpdate(_id, content, { new: true }, (error, docs) => {
+        if (docs == null) {
+            response
+                .status(500)
+                .send(`There was a problem with your update. It didn't go through.`);
+        } else {
+            response.status(200).send(`Updated Contact: ${docs}`);
         }
-    )
-
+    });
 }
-
 
 //function for deleting a contact
-function deleteContact (request, response) {
-
+function deleteContact(request, response) {
     const contactID = request.params.id;
 
-    contact.findByIdAndRemove(contactID, (err) => {
-            if (err) {
-                response.error(err);
-            }
+    contact.findByIdAndRemove(contactID, (err, docs) => {
+        if (!docs) {
+            response.status(500).send('There was a problem with your delete request.');
+        } else if (!err) {
+            response.status(200).send(`Successfully Deleted Contact ID: ${contactID}`);
+        }
     });
-
-    response.status(200).send(`Successfully Deleted Contact ID: ${contactID}`);
-
 }
-
-
 
 module.exports = { getAll, getOne, createContact, updateContact, deleteContact };
--- routes/contacts.js
+++ routes/contacts.js
@@ -2,84 +2,20 @@
 const express = require('express');
 const route = express.Router();
 let contactsController = require('../controllers/contacts');
-// const contact = require('../models/contact');
-
-// load model
-// let contact = require('../models/contact');
-
-// create variable for ObjectId use
-// const { ObjectId } = require('mongodb');
 
 // route endpoint for displaying All contacts
 route.get('/', contactsController.getAll);
-    
-//     const results = connect.getCollection().find();
-    
-//     results.toArray().then((documents) =>  {
-//         res.status(200).json(documents);
-//     });
-// });
 
 // route endpoint for displaying a Specific contact
 route.get('/:id', contactsController.getOne);
-    
-//     const contactId = new ObjectId(req.params.id);
-//     const results = connect.getCollection().find({_id: contactId});
-    
-//     results.toArray().then((documents) =>  {
-//         res.status(200).json(documents[0]);
-//     });
-    
-// });
 
 //route endpoint for adding a new contact
 route.post('/', contactsController.createContact);
-    
-//     console.log(request.body);
-//     const newContact = new contact(request.body);
-
-//     try {
-//         await newContact.save()
-//         response.status(201).send(newContact);
-//     } catch (err) {
-//         res.status(500).send(err);
-//     }
-
-// });
 
 //route endpoint for updating a contact
 route.put('/:id', contactsController.updateContact);
-    
-//     const {id: _id} = request.params;
-//     const content = request.body;
-
-//     console.log(content);
-
-//     contact.findByIdAndUpdate(_id, content, (err) => {
-//             if (err) {
-//                 response.error(err);
-//             } else {
-//                 response.status(200).send(content);
-//             }
-//         }
-//     )
-
-// });
 
 //route endpoint for updating a contact
 route.delete('/:id', contactsController.deleteContact);
-    
-//     const contactID = request.params.id;
-
-//     contact.findByIdAndRemove(contactID, (err) => {
-//             if (err) {
-//                 response.error(err);
-//             }
-//     });
-
-//     response.status(200).send(`Successfully Deleted Contact: ${contactID}`);
-    
-// });
 
 module.exports = route;
-
