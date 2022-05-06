b6be3696fe6b03985de8544a8041e5c2dc87b63d
Quibly <trippy524@yahoo.com>
2022-05-06 12:18:46 -0600

    formatting corrections

--- db/connect.js
+++ db/connect.js
@@ -8,19 +8,18 @@ const mongoose = require('mongoose');
 let _client;
 let _collection;
 
-
 // initialize database
 const initDB = () => {
     mongoose.connect(process.env.DB_URI, (err, client) => {
         if (err) throw err;
         _client = client;
-        _collection = _client.db.collection("contacts");
+        _collection = _client.db.collection('contacts');
     });
 };
 
 // get data from database
 const getCollection = () => {
     return _collection;
-}
+};
 
 module.exports = { initDB, getCollection };
--- models/contact.js
+++ models/contact.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-let contactSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    favoriteColor: String,
-    birthday: String
-}, { versionKey: false });
+let contactSchema = new Schema(
+    {
+        firstName: String,
+        lastName: String,
+        email: String,
+        favoriteColor: String,
+        birthday: String
+    },
+    { versionKey: false }
+);
 
-module.exports = mongoose.model('Contact', contactSchema, "contacts");
+module.exports = mongoose.model('Contact', contactSchema, 'contacts');
