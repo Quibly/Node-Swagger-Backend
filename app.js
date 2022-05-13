9eec21ca651ce06294ccf4d28a9fe12a0c666a4b
Quibly <trippy524@yahoo.com>
2022-05-13 15:05:59 -0600

    error correction

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ server.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 // configure header
 server.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Expose-Headers', 'Content-Disposition');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key, Content-Disposition');
+    // res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
 });
